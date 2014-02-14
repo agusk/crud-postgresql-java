7781deaa63dc77b04d33e0f93393a5fc70049163
Agus Kurniawan <agusk2007@gmail.com>
2014-02-14 19:48:41 +0100

    initial commit

---
+++ src/com/agusk/Main.java
@@ -0,0 +1,8 @@
+package com.agusk;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/com/agusk/PostgreSQLDatabase.java
@@ -0,0 +1,5 @@
+package com.agusk;
+
+
+public class PostgreSQLDatabase {
+}
